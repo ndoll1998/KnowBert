716e2f73e6bf277330c0d96de8c48d70f648b87b
Niclas Doll <niclas@amazonis.net>
2020-08-10 17:47:26 +0200

    Training Knowledge Embedding using TuckER for senticnet graphs

--- senticnet/embedding.py
+++ senticnet/embedding.py
@@ -1,17 +1,24 @@
+import os
+import numpy as np
 # import pytorch
 import torch
 import torch.nn as nn
 
 class Embedding(object):
 
-    def __init__(self, csv_file, embedd_dim, pad_id):
+    def __init__(self, embedd_dim, pad_id):
+        # save parameters
+        self.pad_id, self.embedd_dim = pad_id, embedd_dim
+        # word2id map and embedding
+        self.word2id = None
+        self.embedding:nn.Embedding = None
 
-        self.pad_id = pad_id
+    def load_csv(self, fpath):
+        # clear word2id map
         self.word2id = {}
         # save first position for padding tensor
         # the padding tensor will not be trained or anything since its just a fill vector
-        tensors = [torch.zeros(embedd_dim)]
-
+        tensors = [torch.zeros(self.embedd_dim)]
         # load embedding tensors from file
         with open(csv_file, 'r', encoding='latin-1') as f:
             for row in f:
@@ -23,16 +30,105 @@ class Embedding(object):
                 self.word2id[word] = len(tensors)
                 tensors.append(tensor)
                 # check embedding dimension
-                assert tensors[-1].size(0) == embedd_dim
+                assert tensors[-1].size(0) == self.embedd_dim
 
         # create embedding from loaded tensors
         self.embedding = nn.Embedding(
             num_embeddings=len(tensors),    # add padding tensor
-            embedding_dim=tensors[0].size(0),
+            embedding_dim=self.embedd_dim,
             _weight=torch.stack(tensors, dim=0)
         )
 
+
+    def load(self, words_file, embedd_file):
+        # clear word2id map
+        self.word2id = {}
+        # load word-to-id map
+        with open(words_file, "r") as f:
+            # start enumerating at 1 to skip padding embedding at position 0
+            for i, word in enumerate(f, 1):
+                self.word2id[word] = i
+        # load embedding and add padding embedding
+        weight = torch.load(embedd_file, map_location='cpu')
+        assert weight.size(1) == self.embedd_dim
+        weight = torch.cat((torch.zeros((1, self.embedd_dim)), weight), dim=0)
+        # create embedding
+        self.embedding = nn.Embedding(
+            num_embeddings=weight.size(0),
+            embedding_dim=self.embedd_dim,
+            _weight=weight
+        )
+
     def embedd(self, ids):
+        # make sure embedding is loaded before execution
+        assert self.embedding is not None
         # set all padding ids
         ids[ids == self.pad_id] = 0
         return self.embedding(ids)
+
+
+def train_tuckER_embedding(fpath, dump_path):
+    # imports
+    from graph import SenticNetGraph
+    from pykeen.pipeline import pipeline
+    from pykeen.triples import TriplesFactory
+
+    # load graph
+    g = SenticNetGraph(fpath)
+    # create pseudo-nodes to enucode node attributes
+    pleasent, not_pleasent = len(g.concepts), len(g.concepts) + 1
+    sensitiv, not_sensitive = len(g.concepts) + 2, len(g.concepts) + 3
+    # build triples
+    triples = []
+    for c in g.concepts:
+        # actual connections
+        triples.extend(([c.index, 'semantic', j] for j in g.get_semantic_ids(c)))
+        # encode attributes by binning
+        if c.pleasentness != 0:
+            triples.append([c.index, 'pleasent', pleasent if c.pleasentness > 0 else not_pleasent])
+        if c.sensitivity != 0:
+            triples.append([c.index, 'sensitiv', sensitiv if c.sensitivity > 0 else not_sensitive])
+    triples, n = np.asarray(triples), len(triples)
+    print("Number of Triples (Train/Total): %i/%i" % (n, int(0.8 * n)))
+    # create mask for training and testing separation
+    train_mask = np.full(n, False)
+    train_mask[:int(n * 0.8)] = True
+    np.random.shuffle(train_mask)
+    # separate into training and testing
+    train_triples = triples[train_mask]
+    test_triples = triples[~train_mask]
+    # create triples factories
+    train_factory = TriplesFactory(triples=train_triples)
+    test_factory = TriplesFactory(triples=test_triples)
+    # create and run pipeline
+    print("Training Embedding...")
+    results = pipeline(
+        # data
+        training_triples_factory=train_factory,
+        testing_triples_factory=test_factory,
+        # model
+        model="TuckER"
+    )
+    print("Saving Embeddings...")
+    # save entity and relation embeddings
+    torch.save(results.model.entity_embeddings.weight, os.path.join(dump_path, "entities.bin"))
+    torch.save(results.model.relation_embeddings.weight, os.path.join(dump_path, "relations.bin"))
+    # save words in order matching the embeddings
+    with open(os.path.join(dump_path, "entities.txt"), "w+", encoding='utf-8') as f:
+        f.write('\n'.join([c.text for c in g.concepts]))
+    # done with all
+    print("Saved Embeddings tp %s!" % dump_path)
+
+if __name__ == '__main__':
+    # train a knowledge graph embedding for senticnet graph
+    train_tuckER_embedding(
+        "../data/senticnet/german/senticnet_de.rdf.xml", 
+        "../data/senticnet/german/"
+    )
+    print("\nTrying to load new embedding...")
+    e = Embedding(embedd_dim=200, pad_id=0)
+    e.load(
+        "../data/senticnet/german/entities.txt",
+        "../data/senticnet/german/entities.bin"
+    )
+    print("Everything worked as intended!")
--- senticnet/graph.py
+++ senticnet/graph.py
@@ -2,18 +2,34 @@
 import xml.etree.ElementTree as ET
 from tqdm import tqdm
 
+def parse_and_get_ns(file):
+    events = "start", "start-ns"
+    root = None
+    ns = {}
+    for event, elem in ET.iterparse(file, events):
+        if event == "start-ns":
+            ns[elem[0] if len(elem[0]) > 0 else 'sentic'] = elem[1]
+        elif event == "start":
+            if root is None:
+                root = elem
+    return ET.ElementTree(root), ns
+
 class SenticNetConcept(object):
     """ A Senticnet Concept holding all relevant information about it """
 
-    def __init__(self, child, i):
+    def __init__(self, child, i, ns):
         self.index = i
         # extract from child
-        self.url = child.attrib["{%s}about" % SenticNetGraph.ns['rdf']]
-        self.text = child.find("sentic:text", SenticNetGraph.ns).text.replace(' ', '_')
+        self.url = child.attrib["{%s}about" % ns['rdf']]
+        self.text = child.find("sentic:text", ns).text.replace(' ', '_')
+        # get sentic attributes
+        sentics = child.find("sentic:sentics", ns)
+        self.pleasentness = float(sentics.find("sentic:pleasantness", ns).text)
+        self.sensitivity = float(sentics.find("sentic:sensitivity", ns).text)
         # get semantics
         self.semantic_urls = [
-            s.attrib['{%s}resource' % SenticNetGraph.ns['rdf']]        
-            for s in child.find("sentic:semantics", SenticNetGraph.ns)
+            s.attrib['{%s}resource' % ns['rdf']]        
+            for s in child.find("sentic:semantics", ns)
         ]
 
 class SenticNetGraph(object):
@@ -21,28 +37,26 @@ class SenticNetGraph(object):
         Expecting .rdf.xml file
     """
 
-    # xml namespaces
-    ns = {
-        'rdf': "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-        'sentic': "http://sentic.net"
-    }
-
     def __init__(self, path:str):
+        print("Loading SenticNet Graph...", end=" ")
         # load xml file and get root
-        tree = ET.parse(path)
+        tree, self.ns = parse_and_get_ns(path)
         root = tree.getroot()
         # get list of all concepts and create a mapping from url to concept
-        self.concepts = [SenticNetConcept(child, i) for i, child in enumerate(root)]
+        self.concepts = [SenticNetConcept(child, i, self.ns) for i, child in enumerate(root)]
         self.url2concept_id = {c.url: c.index for c in self.concepts}
+        self.base_url = '/'.join(self.concepts[0].url.split('/')[:-1])
         # delete xml tree
         del tree, root
+
+        print("Done")
 
     def get_node_from_id(self, i):
         return self.concepts[i]
 
     def get_concept(self, term:str):
         # build url to concept
-        url = "http://sentic.net/api/en/concept/%s" % term
+        url = "%s/%s" % (self.base_url, term)
         # check if url is valid
         if url not in self.url2concept_id:
             return None
@@ -51,4 +65,4 @@ class SenticNetGraph(object):
 
     def get_semantic_ids(self, concept:SenticNetConcept):
         # get semantics from url
-        return [self.url2concept_id[url] for url in concept.semantic_urls]
+        return [self.url2concept_id[url] for url in concept.semantic_urls if url in self.url2concept_id]
